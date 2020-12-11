50e4f1c65574d312836843c50bdb1f728bb8f76c
zimzim <zimzim@youcanstar.com>
2020-12-11 15:54:39 +0900

    add validation on article model

--- server/Article/article.js
+++ server/Article/article.js
@@ -6,23 +6,30 @@ export default function makeArticle({
   publishedOn,
   content,
   image = "",
+  hash = "",
 } = {}) {
-  if (!_id) _id = generateId();
   if (!title) throw new Error("Article must have Title");
-  if (!publishedOn) throw new Error("Article must have creation date");
+  if (!publishedOn) throw new Error("Article must have published date");
   if (!content) throw new Error("Article must have content");
-  if (publishedOn && !isValidDate(publishedOn))
-    throw new Error("Article must have valid creation date");
 
-  publishedOn = formatDate(publishedOn);
-  const contentHash = makeContentHash(content);
+  if (title && !isMeaningfulText(title))
+    throw new Error("Title doesn't have meaningful text");
+  if (publishedOn && !isValidDate(publishedOn))
+    throw new Error("Article must have valid published date");
+  if (content && !isMeaningfulText(content))
+    throw new Error("Content doesn't have meaningful text");
+
+  if (!_id) _id = generateId();
+  if (!hash) hash = makeContentHash(content);
+  if (publishedOn) publishedOn = formatDate(publishedOn);
+
   return Object.freeze({
     _id: _id,
     title: title,
     publishedOn: publishedOn,
     content: content,
     image: image,
-    hash: contentHash
+    hash: hash,
   });
 }
 /** NOTE article page's note
@@ -31,10 +38,11 @@ export default function makeArticle({
  * 2. validate all data
  * 3. non-valid article cannot never be saved. every format-error will be thrown here
  * 4. create object and return
+ * only: make valid article, no duplicate check
  *
  * Mandatory field: id, title, publishedOn, content
  * id must be unique
- * title must be unique -> can change.. but shouldn't it be?
+ * title must be unique -> can change.. but shouldn't it be? -> not here, will be checked outside
  * published date must be valid ISO 8601(yyyy-mm-dd hh:mm:ss)
  * content must be unique
  * image must be unique within the article
@@ -48,6 +56,11 @@ function isValidDate(str) {
   const date = new Date(str);
   return !isNaN(date);
 }
+function isMeaningfulText(str){
+ const getText = str.replace(/\s/g, '');
+ if(getText.length > 0) return true;
+ return false;
+}
 function formatDate(date) {
   date = new Date(date);
   const currentTime = new Date();
@@ -56,6 +69,6 @@ function formatDate(date) {
   const converted = getDate.toISOString().replace("T", " ").split(".")[0];
   return converted;
 }
-function makeContentHash(content){
-    return md5(content)
+function makeContentHash(content) {
+  return md5(content);
 }
