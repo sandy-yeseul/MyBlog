e56ea1d0cdd6012c720a5848b0eaa413b4d59f2b
zimzim <zimzim@youcanstar.com>
2020-12-15 17:28:53 +0900

    list updated with flat lis

--- mobile/app/screens/List.js
+++ mobile/app/screens/List.js
@@ -4,26 +4,36 @@ import { Div, Text, List, ScrollDiv } from "../components/atoms";
 import { COLORS, theme, images } from "../../constants";
 export default () => {
   const [list, setList] = useState([
-    { title: "1", PublishedOn: "2020-01-01" },
-    { title: "style", PublishedOn: "2020-03-11" },
-    { title: "What We want to do", PublishedOn: "2020-08-09" },
-    { title: "Output", PublishedOn: "2020-01-20" },
-    { title: "Flex", PublishedOn: "2020-10-18" },
-    { title: "Messages", PublishedOn: "2020-12-21" },
-    { title: "Star", PublishedOn: "2020-12-31" },
+    { key: "1", title: "1", PublishedOn: "2020-01-01" },
+    { key: "2", title: "style", PublishedOn: "2020-03-11" },
+    { key: "3", title: "What We want to do", PublishedOn: "2020-08-09" },
+    { key: "4", title: "Output", PublishedOn: "2020-01-20" },
+    { key: "8", title: "Flex", PublishedOn: "2020-10-18" },
+    { key: "71", title: "Messages", PublishedOn: "2020-12-21" },
+    { key: "18", title: "Star", PublishedOn: "2020-12-31" },
   ]);
   return (
-    <ScrollDiv>
-      <Div style={styles.container1}>
-        {list.map((item, i) => {
-          return (
-            <Text key={i} style={styles.item}>
-              {item.PublishedOn} :: {item.title}
-            </Text>
-          );
-        })}
-      </Div>
-    </ScrollDiv>
+    // <ScrollDiv>
+    //   <Div style={styles.container1}>
+    //     {list.map((item, i) => {
+    //       return (
+    //         <Text key={i} style={styles.item}>
+    //           {item.PublishedOn} :: {item.title}
+    //         </Text>
+    //       );
+    //     })}
+    //   </Div>
+    // </ScrollDiv>
+    <Div style={styles.container1}>
+      <List
+        data={list}
+        renderItem  ={({item}) => (
+          <Text style={styles.item}>
+            {item.PublishedOn} :: {item.title}
+          </Text>
+        )}
+      />
+    </Div>
   );
 };
 const styles = StyleSheet.create({
