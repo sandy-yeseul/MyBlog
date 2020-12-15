51bd94367b22809c16413c7251153930d5f43983
zimzim <zimzim@youcanstar.com>
2020-12-15 17:57:31 +0900

    initial navigation

---
+++ mobile/navigation/index.js
@@ -0,0 +1,9 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {Landing} from '../app/screens';
+const screens = {
+    Landing: {
+        screen:
+    }
+}
+const HomeStack = createStackNavigator(screens);
