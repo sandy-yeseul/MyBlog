ebdd7078042fda91a35c6fd940f594c1b9b289b1
zimzim <zimzim@youcanstar.com>
2020-12-18 11:58:33 +0900

    fetch data

--- mobile/app/screens/Detail.js
+++ mobile/app/screens/Detail.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { COLORS } from "../../constants";
 import { Div, Text } from "../components/atoms";
 export default ({ route, navigation }) => {
-  // const {id} = route.params;
+  const { id } = route.params;
   // console.log(id)
-  const [Post, setPost] = useState({
-    title: "Physical",
-    publishedOn: "2019-09-08",
-    content: "Common love isn't for us. \n we created something phenomenon",
-  });
+  const [Post, setPost] = useState();
+  const getData = async () => {
+    try {
+      const result = await fetch(
+        `http://192.168.124.64:2020/api/articles/${id}`
+      );
+      const resJson = await result.json();
+      setPost(resJson)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <Div style={styles.container}>
-      <Text style={styles.title}>{Post.title}</Text>
-      <Text style={styles.date}>{Post.publishedOn}</Text>
-      <Text style={styles.content}>{Post.content}</Text>
-    </Div>
+    <>
+      {Post && (
+        <Div style={styles.container}>
+          <Text style={styles.title}>{Post.title}</Text>
+          <Text style={styles.date}>{Post.publishedOn}</Text>
+          <Text style={styles.content}>{Post.content}</Text>
+        </Div>
+      )}
+    </>
   );
 };
 const styles = StyleSheet.create({
