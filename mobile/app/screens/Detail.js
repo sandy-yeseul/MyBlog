2c5ee890f5bde71ece2d00acc1038e83d8baff54
zimzim <zimzim@youcanstar.com>
2020-12-18 14:27:07 +0900

    initial layout without scroll view

--- mobile/app/screens/Detail.js
+++ mobile/app/screens/Detail.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { COLORS } from "../../constants";
-import { Div, Text } from "../components/atoms";
+import { Div, Image, Text } from "../components/atoms";
+import { theme } from "../../constants";
 export default ({ route, navigation }) => {
   const { id } = route.params;
-  // console.log(id)
   const [Post, setPost] = useState();
   const getData = async () => {
     try {
@@ -12,7 +12,8 @@ export default ({ route, navigation }) => {
         `http://192.168.124.64:2020/api/articles/${id}`
       );
       const resJson = await result.json();
-      setPost(resJson)
+      const data = formatData(resJson);
+      setPost(data);
     } catch (err) {
       console.log(err);
     }
@@ -24,13 +25,23 @@ export default ({ route, navigation }) => {
     <>
       {Post && (
         <Div style={styles.container}>
-          <Text style={styles.title}>{Post.title}</Text>
-          <Text style={styles.date}>{Post.publishedOn}</Text>
-          <Text style={styles.content}>{Post.content}</Text>
+          <Div style={styles.imageContainer}>
+            <Image source={{ uri: Post.image }} style={styles.image} />
+          </Div>
+          <Div style={styles.textContainer}>
+            <Text style={styles.title}>{Post.title}</Text>
+            <Text style={styles.date}>{Post.publishedOn}</Text>
+            <Text style={styles.content}>{Post.content}</Text>
+          </Div>
         </Div>
       )}
     </>
   );
+};
+const formatData = (article) => {
+  const _article = { ...article };
+  if (!_article.image) _article.image = "https://picsum.photos/300";
+  return _article;
 };
 const styles = StyleSheet.create({
   container: {
@@ -39,16 +50,32 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "flex-start",
   },
+  imageContainer: {
+    flex: 1,
+    width: '100%'
+  },
+  textContainer: {
+    flex: 1,
+    padding: theme.SIZES.base,
+  },
   title: {
     flex: 1,
-    fontWeight: "900",
-    fontSize: 50,
-    color: "red",
+    fontWeight: "600",
+    fontSize: 35,
   },
   date: {
     flex: 0.5,
+    paddingTop: theme.SIZES.base,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+    alignSelf: 'flex-start', // make width max-content,
   },
   content: {
     flex: 4,
+    paddingTop: theme.SIZES.padding,
+  },
+  image: {
+    width: "100%",
+    height: '100%',
   },
 });
